ce529271bf9ff2f725eb17b0cef033a17a92c2a4
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-05-11 21:37:17 +0100

    added api routes and handled redirections

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('login', function () {
+    return response()->json(['status' => 'error', 'message' => 'Not found'], 404);
+})->name('login');
+
 Route::get('/', function () {
     return view('welcome');
 });
